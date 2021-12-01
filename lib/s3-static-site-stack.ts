e4252bd0ab168c73ee5eb19cbe05b0c81d4987f9
Martin <mart256@gmail.com>
2021-12-01 22:48:23 +0100

    Creating the bucket.

--- lib/s3-static-site-stack.ts
+++ lib/s3-static-site-stack.ts
@@ -1,9 +1,26 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3_deploy from '@aws-cdk/aws-s3-deployment';
+
+
+interface BucketConfig {
+  indexDocument: string;
+  errorDocument: string;
+  publicReadAccess: boolean;
+}
+
+const bucketName = 'LearningCDKPatternsBucket';
 
 export class S3StaticSiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const bucketConf: BucketConfig = {
+      indexDocument: 'index.html',
+      errorDocument: 'error.html',
+      publicReadAccess: true
+    }
 
+    const bucket = new s3.Bucket(this, 'WebsiteBucket', bucketConf);
   }
 }
