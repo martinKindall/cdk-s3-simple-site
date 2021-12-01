17f7d830545cf8f83cb7e5d45c6852c5cfd35cc1
Martin <mart256@gmail.com>
2021-12-01 22:40:05 +0100

    Initial commit

---
+++ test/s3-static-site.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as S3StaticSite from '../lib/s3-static-site-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3-static-site-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3StaticSite.S3StaticSiteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
